e87e45cdf7a035b307c3ca0853eb41d21b32081d
石井日向 <hyu825@gmail.com>
2024-04-17 20:20:21 +0900

    config/application.rbにリンク先を追加

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module Myapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.hosts << "learning-record-app.onrender.com"
   end
 end
