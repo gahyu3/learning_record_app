af48febd6e818b71e2b9d4ff2ba3ad98bcab2204
石井日向 <hyu825@gmail.com>
2024-04-25 21:52:24 +0900

    formのエラーメッセージ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable
 
   validates :name, presence: { message: 'は必ず入力してください' }, length: { maximum: 255, message: 'は255文字以内で入力してください'}
-  validates :email, presence: { message: 'は必ず入力してください' }, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'が正しい形式ではありません' }
+  validates :email, presence: { message: 'は必ず入力してください' }, uniqueness: true, format: { with: /[^@]+@[^@]+\.[^@]+/, message: 'が正しい形式ではありません' }
   validates :password, presence: { message: 'は必ず入力してください' }, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d).{6,}\z/, message: 'は英数字8文字以上で入力してください' }
 
 end
